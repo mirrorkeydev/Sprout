f41707490777f337d10f9d24558213e6cd6479a2
mirrorkeydev <mirrorkeydev@gmail.com>
2020-09-04 15:10:45 -0700

    Add setup image to About page and change layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,26 @@
     <HeadingTitle msg="SPROUT"/>
     <div class="about">
       <SectionHeader text="about" />
-      <p :class="$mq">
-        Sprout was made by <a href="https://github.com/mirrorkeydev">Melanie Gutzmann</a>
-        and <a href="https://github.com/prototypicalpro">Noah Koontz</a>. The project
-        is inspired by (and connected to) Melanie's plant collection, pictured below.
-      </p>
+      <div id="main-wrapper">
+        <div class="about-connection">
+          <img id="about-connection-img" :class="$mq" src="/img/plants/plantconnection.jpg" alt="">
+        </div>
+        <p id="text" :class="$mq">
+          Sprout was made by <a href="https://github.com/mirrorkeydev">Melanie Gutzmann</a>
+          and <a href="https://github.com/prototypicalpro">Noah Koontz</a>. The project
+          is inspired by (and connected to) Melanie's plant collection.
+          <br><br>
+          The current setup consists of two "Chirp!" soil moistures connected to an 
+          Azure Sphere MT3620. These collect environmental data every 2 minutes, and
+          publish the data to a MongoDB instance hosted on Atlas every 10 minutes.
+          This site queries the data through an AWS Lambda API.
+        </p>
+        <div id="image-grid-wrapper" :class="$mq">
+          <img class="image-grid-img" v-for="(image, i) in images" :key="i" :src="image" alt="" :class="$mq">
+        </div>
+      </div>
     </div>
-    <div id="image-grid-wrapper" :class="$mq">
-      <img v-for="(image, i) in images" :key="i" :src="image" alt="" :class="$mq">
-    </div>
+
   </div>
 </template>
 
@@ -27,7 +38,7 @@ export default {
   },
   data: function () {
     return {
-      images: new Array(11).fill(0).map((e, i) => `./img/plants/plant${i+1}.jpg`),
+      images: new Array(10).fill(0).map((e, i) => `./img/plants/plant${i+2}.jpg`),
     };
   },
 };
@@ -39,41 +50,73 @@ export default {
 }
 h1 {
   display: inline-block;
-  margin: 0px 0px 40px 0px;
+  margin: 0 0 40px 0;
 }
-p {
-  margin: 0vw 30vw 40px 30vw
-}
-p.mobile{
-  margin: 0vw 10vw 40px 10vw
-}
-img {
+.image-grid-img {
   display: inline-block;
   border-radius: 5px;
   margin: 10px;
-  max-height: 300px;
+  max-height: 200px;
 }
-img.tablet {
- max-height: 225px;
+.image-grid-img.tablet {
+ max-height: 175px;
  margin: 5px;
 }
-img.mobile {
- max-height: 150px;
+.image-grid-img.mobile {
+ max-height: 125px;
  margin: 5px;
 }
 #image-grid-wrapper{
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  margin: 0px 20vw 0px 20vw;
+  flex: 1 0 700px;
+  margin: 10px;
 }
 #image-grid-wrapper.tablet{
+  flex: 1 0 500px;
   margin: 0px 10vw 0px 10vw;
 }
 #image-grid-wrapper.mobile{
-  margin: 0px 0vw 0px 0vw;
+  flex: 1 0 350px;
+  margin: 0;
 }
 a:link, a:visited, a:hover, a:active {
-    text-decoration: none;
+  text-decoration: none;
+}
+.about-connection {
+  flex: 1 1 300px;
+}
+#about-connection-img{
+  height: 600px;
+  margin: 10px;
+}
+#about-connection-img.tablet{
+  height: 500px;
+}
+#about-connection-img.mobile{
+  height: 400px;
+}
+#main-wrapper{
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 10vw 100px 10vw;
+}
+#text {
+  display: initial;
+  flex: 1 1 100px;
+  padding: 1vw;
+  margin: 1vw;
+  min-width: 150px;
+}
+#text.tablet {
+  padding: 15px;
+  margin: 15px;
+}
+#text.mobile {
+  padding: 10px 0;
+  margin: 10px 0;
 }
 </style>
